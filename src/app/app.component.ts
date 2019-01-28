00fc33d1e80023e2da3e03c6aeb0fa5890fab87d
Chauhan Kirankumar <kirankumar.chauhan@tieto.com>
2019-01-28 18:32:43 +0530

    Input with two way data added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  // tslint:disable-next-line:component-selector
+  selector: 'ng6-widget',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular Element';
+  title = 'Angular Element Sample';
+  public textBox = '';
 }
